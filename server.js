563d4223323d28d662d8f56b3f5935d1b3f70c0e
Santosh Mohan Saladi <vsaladi@uncc.edu>
2023-09-14 16:37:58 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = { 
+    myBudget: [
+    {
+        title: 'Eat Out',
+        budget: 30
+    },
+    {
+        title: 'Rent',
+        budget: 350
+    },
+    {
+        title: 'Groceries',
+        budget: 90
+    }
+]};
+
+app.get('/hello', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get('/budget', (req, res) => {
+    res.send(budget);
+});
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:3000');
+});
